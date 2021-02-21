4567ef4aa3ac89cb5bebb2a906fc7e0f9e467058
ADAM ANDREASON <adamandreason@gmail.com>
2021-02-20 21:38:47 -0700

    major l33t trans refactor for compatability

--- index.js
+++ index.js
@@ -54,59 +54,62 @@ const vowelShifter = (str, vowel) => {
 const l33tTranslator = (theString) => {
   if (typeof theString !== "string") return "Please provide a string, d00d."
   let theL33t = theString.toUpperCase()
-    .replaceAll("DUDE", "D00D")
-    .replaceAll("PORN", "PRON")
-    .replaceAll("GIRL", "GURL")
-    .replaceAll(" HI ", " HAI ")
-    .replaceAll("YOU", "J00")
-    .replaceAll("COOL", "KEWL")
-    .replaceAll("OKAY", "KK")
-    .replaceAll("OK", "KK")
-    .replaceAll("SMART", "SMAT")
-    .replaceAll("MAX", "MAX0R")
-    .replaceAll("OWN", "PWN")
-    .replaceAll("FEAR", "PH33R")
-    .replaceAll("THE", "T3H")
-    .replaceAll("M0R3", "MOAR")
-    .replaceAll("HACKER", "H4X0R")
-    .replaceAll("CENSORED", "C3N50RED")
-    .replaceAll("YAY", "W00T")
-    .replaceAll("WOOHOO", "W00T")
-    .replaceAll("GOOD LUCK", "GL")
-    .replaceAll(" HA! ", " R0FZ ")
-    .replaceAll("WAS", "WUZ")
-    .replaceAll("CKS", "X")
-    .replaceAll("S ", "Z ")
-    .replaceAll("S.", "Z.")
-    .replaceAll("S!", "Z!")
-    .replaceAll("S?", "Z?")
-    .replaceAll("SZ", "SS")
-    .replaceAll("ED ", "'D ")
-    .replaceAll("ED.", "'D.")
-    .replaceAll("A", "@")
-    .replaceAll("E", "3")
-    .replaceAll("I", "1")
-    .replaceAll("O", "0")
-    .replaceAll("!", "!!!")
-    .replaceAll("Y", "¥")
-    .replaceAll("NO WAY", "NOWAI")
-    .replaceAll("S33 Y0U", "CYA")
-    .replaceAll("Y3S", "YASS")
-    .replaceAll("P0W3R", "POWWAH")
-    .replaceAll("W1NN3R", "WINNAR")
-    .replaceAll("L33T SUX", "L33T RULZ")
-    .replaceAll("RUL3S", "RULZ")
-    .replaceAll("L0L", "LOPL")
-    .replaceAll("3L1T3", "L33T")
-    .replaceAll("L1K3", "LIEK")
-    .replaceAll("@ND", "ND")
-    .replaceAll("B¥3", "BAI")
-    .replaceAll("M3 T00", "M2")
-    .replaceAll("W0RST", "WURST");
+    .replace(/DUDE/g, "D00D")
+    .replace(/YOU/g, "J00")
+    .replace(/OKAY/g, "KK")
+    .replace(/OK/g, "KK")
+    .replace(/OWN/g, "PWN")
+    .replace(/FEAR/g, "PH33R")
+    .replace(/THE/g, "T3H")
+    .replace(/HACKER/g, "H4X0R")
+    .replace(/CENSORED/g, "C3N50R3D")
+    .replace(/CENSOR/g, "C3N50R")
+    .replace(/YAY/g, "W00T")
+    .replace(/WOOHOO/g, "W00T")
+    .replace(/GOOD LUCK/g, "GL")
+    .replace(/HA! /g, " R0FZ ")
+    .replace(/WAS/g, "WUZ")
+    .replace(/CKS/g, "X")
+    .replace(/S /g, "Z ")
+    .replace(/S\./g, "Z.")
+    .replace(/S!/g, "Z!")
+    .replace(/S\?/g, "Z?")
+    .replace(/SZ/g, "SS")
+    .replace(/ED /g, "'D ")
+    .replace(/ED\./g, "'D.")
+    .replace(/A/g, "@")
+    .replace(/E/g, "3")
+    .replace(/I/g, "1")
+    .replace(/O/g, "0")
+    .replace(/!/g, "!!!")
+    .replace(/Y/g, "¥")
+    .replace(/N0 W@¥/g, "NOWAI")
+    .replace(/S33 ¥0U/g, "CYA")
+    .replace(/¥3S/g, "YASS")
+    .replace(/P0W3R/g, "POWWAH")
+    .replace(/W1NN3R/g, "WINNAR")
+    .replace(/L33T SUX/g, "L33T RULZ")
+    .replace(/RUL3S/g, "RULZ")
+    .replace(/L0L/g, "LOPL")
+    .replace(/3L1T3/g, "L33T")
+    .replace(/L1K3/g, "LIEK")
+    .replace(/@ND/g, "ND")
+    .replace(/B¥3/g, "BAI")
+    .replace(/M3 T00/g, "M2")
+    .replace(/W0RST/g, "WURST")
+    .replace(/1Z/g, "1S")
+    .replace(/N3'D/g, "N33D")
+    .replace(/C00L/g, "KEWL")
+    .replace(/SM@RT/g, "SMAT")
+    .replace(/MAX/g, "MAX0R")
+    .replace(/M0R3/g, "MOAR")
+    .replace(/P0RN/g, "PRON")
+    .replace(/G1RL/g, "GURL")
+    .replace(/ H1 /g, " HAI ")
   return theL33t;
 };
 
-console.log(l33tTranslator("Translate this, please."))
+console.log(l33tTranslator("Wow, your are the worst. I need to no way and"))
 //returns how many days until Christmas
 const tilChristmas = () => {
   let thePresentTime = new Date();
@@ -145,10 +148,10 @@ const searchAnime = (searchTerm) => {
   return searchedAnime;
 };
 
-module.exports = {
-  tryCatch,
-  vowelShifter,
-  l33tTranslator,
-  tilChristmas,
-  searchAnime,
-}
+// module.exports = {
+//   tryCatch,
+//   vowelShifter,
+//   l33tTranslator,
+//   tilChristmas,
+//   searchAnime,
+// }
